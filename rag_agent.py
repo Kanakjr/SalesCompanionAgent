44ce4c8380dcaea773dc9a7c311413fb8d7c37bc
Kanakjr <ksdusa4@gmail.com>
2024-04-11 19:55:55 +0530

    Refactor rag_agent.py and sales_companion.py

--- rag_agent.py
+++ rag_agent.py
@@ -1,19 +1,49 @@
 from langchain_community.vectorstores import Chroma
 from llm import load_llm, load_openai_embeddings
 
-vector_db = Chroma(
+vectordb_interaction_notes = Chroma(
     collection_name="interaction_notes",
     persist_directory="./data/vectorstore/",
     embedding_function=load_openai_embeddings(),
 )
 
+vectordb_hcp_names = Chroma(
+    collection_name="hcp_names",
+    persist_directory="./data/vectorstore/",
+    embedding_function=load_openai_embeddings(),
+)
+
+
+
+def get_interaction_notes(query, SalesRep_ID=None, HCP_ID=None):
+    # Create filter based on SalesRep_ID and HCP_ID. If both are None, filter is empty
+    # if both are given use and operator. If either is given use or operator
+    if SalesRep_ID and HCP_ID:
+        filter = {"$and": [{"SalesRep_ID": {"$eq": SalesRep_ID}}, {"HCP_ID": {"$eq": HCP_ID}}]}
+    elif SalesRep_ID:
+        filter = {"SalesRep_ID": {"$eq": SalesRep_ID}}
+    elif HCP_ID:
+        filter = {"HCP_ID": {"$eq": HCP_ID}}
+    else:
+        filter = {}
+    docs = vectordb_interaction_notes.similarity_search(query, filter=filter)
+    if docs:
+        return docs
+    else:
+        return "No document found"
+    
+def get_hcp_names(query,filter={}):
+    docs = vectordb_hcp_names.similarity_search(query, filter=filter)
+    if docs:
+        return docs
+    else:
+        return "No document found"
+
 if __name__ == "__main__":
     query = "follow-up video call with Dr. Morgan Murphy"
-    filter = {'$and': [{'SalesRep_ID': {'$eq': "T06"}},
-                    {'HCP_ID': {'$eq': "HCP002"}}]}
-    docs = vector_db.similarity_search(query,filter=filter)
-    if docs:
-        print(docs[0].page_content)
-    else:
-        print("No document found")
     
+    # interaction_notes = get_interaction_notes(query, SalesRep_ID="T09", HCP_ID="HCP035")
+    # print(f"Interaction Notes: {interaction_notes}")
+
+    # hcp_name = get_hcp_names(query)
+    # print(f"HCP Name: {hcp_name}")
--- sales_companion.py
+++ sales_companion.py
@@ -1,7 +1,9 @@
+from rag_agent import get_hcp_names
 from structured import StructuredDatabase
 from sql_agent import SQLAgent
 from llm import load_llm
 from dotenv import load_dotenv
+from langchain_core.prompts import ChatPromptTemplate
 
 load_dotenv("./.env")
 structured_database_uri = "sqlite:///./data/SalesAssistant.db"
@@ -21,7 +23,26 @@ class SalesCompanion:
         if len(df) == 1:
             return df.to_dict(orient="records")[0]
         return None
-
+    
+    def get_hcp_details(self, query):
+        hcp_names = get_hcp_names(query)
+        if hcp_names != "No document found":
+            prompt = ChatPromptTemplate.from_template(
+                """Given user query: {query}
+                and a list of HCP names: {hcp_names}
+                Figure out if the user query is related to any of the HCP names provided. If so, return only the HCP Name as output.
+                If not, return 'No match found' as output.'
+                output:"""
+            )
+            # Chain the prompt template with the language model for processing
+            chain = prompt | self.llm
+            response = chain.invoke({"query": query, "hcp_names": hcp_names}).content
+            if response != "No match found":
+                hcp_details = self.db.df_from_sql_query(f'SELECT * FROM HCP where HCP_Name = "{response}"').to_dict(orient="records")
+                if len(hcp_details) == 1:
+                    return hcp_details[0]
+        return None
+    
     def invoke(self, input):
         return self.sql_agent.invoke(input=input)
 
@@ -31,10 +52,16 @@ class SalesCompanion:
 
 if __name__ == "__main__":
     sales_companion = SalesCompanion(useremail="john.doe@example.com")
-    print(sales_companion.user_info)
+    print(f"User Info: {sales_companion.user_info}")
+
+    hcp_details = sales_companion.get_hcp_details("follow-up video call with Dr. Luffy")
+    print(f"HCP Details: {hcp_details}")
+
+    hcp_details = sales_companion.get_hcp_details("follow-up video call with Dr. Morgan Murphy")
+    print(f"HCP Details: {hcp_details}")
 
     # response = sales_companion.invoke("How am I performing against my goals?")
     # print(response)
 
-    response = sales_companion.invoke("Can you Draft an email to Tanya Lewis?")
-    print(response)
+    # response = sales_companion.invoke("Can you Draft an email to Tanya Lewis?")
+    # print(response)
