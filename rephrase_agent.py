abdc858f9f378a2d0cd39274c5ce2aa2d41129e6
Kanakjr <ksdusa4@gmail.com>
2024-04-13 17:29:37 +0530

    Add rephrase_agent.py for rephrasing user queries based on conversation history

---
+++ rephrase_agent.py
@@ -0,0 +1,34 @@
+from langchain_core.pydantic_v1 import BaseModel, Field
+from llm import load_llm
+from dotenv import load_dotenv
+from langchain.prompts import PromptTemplate
+from examples import examples
+from langchain_core.output_parsers import StrOutputParser
+from langchain.schema.runnable.config import RunnableConfig
+
+
+def rephrase_question(query,conversation_history,callbacks=[]):
+    # 
+    prompt = PromptTemplate(
+    template= """Given user input: {query}
+Conversation History: {conversation_history}
+Rephrase the user query based on convesation history to make it more clear. 
+Rephrased Query:""",
+    input_variables=["query", "conversation_history"],
+)
+    # Chain the prompt template with the language model for processing
+    chain = prompt | load_llm() | StrOutputParser()
+
+    response = chain.invoke({"query": query,
+                             "conversation_history":conversation_history,
+                             },
+                             config=RunnableConfig(run_name='rephrase_question',
+                                                   callbacks=callbacks)
+                            )
+    return response
+
+if __name__ == "__main__":
+    # Conversation History - Examples
+    query = "How many prescription has been written by Crystal Esparza?"
+    conversation_history = ''
+    print(rephrase_question(query,conversation_history))
