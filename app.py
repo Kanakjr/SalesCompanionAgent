c894030f49ceefbc359977d027b640e13b4d922d
Kanakjr <ksdusa4@gmail.com>
2024-04-14 00:23:57 +0530

    Refactor app.py to support dynamic selection of LLM model based on chat profile

--- app.py
+++ app.py
@@ -1,9 +1,40 @@
+import os
 import chainlit as cl
 from sales_companion import SalesCompanion
 
+def set_environment_variables(version):
+    suffix = "_1" if version == "GPT-3.5" else "_2"
+    os.environ["OPENAI_MODEL_NAME"] = os.environ[f"OPENAI_MODEL_NAME{suffix}"]
+    if os.environ.get("GENAI_TYPE") == 'AzureOpenAIChat':
+        os.environ["AZURE_ENDPOINT"] = os.environ[f"AZURE_ENDPOINT{suffix}"]
+        os.environ["OPENAI_DEPLOYMENT_NAME"] = os.environ[f"OPENAI_DEPLOYMENT_NAME{suffix}"]
+        os.environ["OPENAI_API_KEY"] = os.environ[f"OPENAI_API_KEY{suffix}"]
+
+@cl.set_chat_profiles
+async def chat_profile():
+    return [
+        cl.ChatProfile(
+            name="GPT-3.5",
+            markdown_description="The underlying LLM model is **GPT-3.5**.",
+        ),
+        cl.ChatProfile(
+            name="GPT-4",
+            markdown_description="The underlying LLM model is **GPT-4**.",
+        ),
+    ]
 
 @cl.on_chat_start
 def start():
+    ## Set the llm model based on the chat profile    
+    chat_profile = cl.user_session.get("chat_profile")
+    if chat_profile == "GPT-3.5":
+        set_environment_variables("GPT-3.5")
+    elif chat_profile == "GPT-4":
+        set_environment_variables("GPT-4")
+    print(f"Chat Profile: {chat_profile}")
+    print(f"MODEL_NAME: {os.environ.get('OPENAI_MODEL_NAME')}")
+
+    ## Initialize the Sales Companion
     sales_companion = SalesCompanion(useremail="kanak.dahake@example.com")
     cl.user_session.set("sales_companion", sales_companion)
 
